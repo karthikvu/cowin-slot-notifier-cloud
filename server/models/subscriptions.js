254c55aa3e4758dbf421a3c0870686f023e3a2a9
Karthik VU <karthik.vu@live.com>
2021-05-22 10:57:17 +0530

    Query Update

--- server/models/subscriptions.js
+++ server/models/subscriptions.js
@@ -1,14 +1,31 @@
-const { db } = require('../lib/database')
+const { db } = require("../lib/database");
 
-const findAll = () => db.any('SELECT * FROM subscriptions')
-const findByUserId = (id) => db.any('SELECT * FROM subscriptions where userid = $1', id)
-const findByName = name => db.any('SELECT * FROM subscriptions where name = $1', name)
-const findByBUNumber = email => db.any('SELECT * FROM subscriptions where email = $1', email)
-const findByPhone = phone => db.any('SELECT * FROM subscriptions where phone = $1 or phoneAlt = $1', phone)
-const updateNotifCount = (userid, pincode, age) => db.any('UPDATE subscriptions SET "notifCount" = "notifCount" + 1 where userid = $1 and pincode = $2 and age = $3', [userid, pincode, age])
-const resetNotifCount = (userid, pincode, age) => db.any('UPDATE subscriptions SET "notifCount" = 0 where userid = $1 and pincode = $2 and age = $3', [userid, pincode, age])
+const findAll = () =>
+  db.any("SELECT * FROM subscriptions where active = true and deleted = false");
+const findByUserId = (id) =>
+  db.any("SELECT * FROM subscriptions where userid = $1", id);
+const findByName = (name) =>
+  db.any("SELECT * FROM subscriptions where name = $1", name);
+const findByBUNumber = (email) =>
+  db.any("SELECT * FROM subscriptions where email = $1", email);
+const findByPhone = (phone) =>
+  db.any(
+    "SELECT * FROM subscriptions where phone = $1 or phoneAlt = $1",
+    phone
+  );
+const updateNotifCount = (userid, pincode, age) =>
+  db.any(
+    'UPDATE subscriptions SET "notifCount" = "notifCount" + 1 where userid = $1 and pincode = $2 and age = $3',
+    [userid, pincode, age]
+  );
+const resetNotifCount = (userid, pincode, age) =>
+  db.any(
+    'UPDATE subscriptions SET "notifCount" = 0 where userid = $1 and pincode = $2 and age = $3',
+    [userid, pincode, age]
+  );
 const create = (subscription) => {
-    return db.query(`INSERT INTO subscriptions (
+  return db.query(
+    `INSERT INTO subscriptions (
         userid,
         name,
         phone,
@@ -34,18 +51,20 @@ const create = (subscription) => {
         now(),
         now(),
         0
-) RETURNING *`, subscription)
-}
-const update = (id, patient) => {}
-const deleteOne = id => {}
+) RETURNING *`,
+    subscription
+  );
+};
+const update = (id, patient) => {};
+const deleteOne = (id) => {};
 
 module.exports = {
-    findAll,
-    findByUserId,
-    findByBUNumber, 
-    findByName, 
-    findByPhone,
-    create,
-    updateNotifCount,
-    resetNotifCount
-}
+  findAll,
+  findByUserId,
+  findByBUNumber,
+  findByName,
+  findByPhone,
+  create,
+  updateNotifCount,
+  resetNotifCount,
+};
